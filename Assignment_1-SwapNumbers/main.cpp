1bf3a5ad084b046b95ab3071e5849d8c47add180
ManlyItalian <zxc5408@g.rit.edu>
2016-08-24 16:26:50 -0400

    Added a function to check the data types, fixed the input selector check and started the file reader function.

--- Assignment_1-SwapNumbers/main.cpp
+++ Assignment_1-SwapNumbers/main.cpp
@@ -15,44 +15,70 @@
 // 2. Add code for swapping other numeric data types other than integers such as float, double. -Done
 // 3. Add Error checking (try, catch and finally)
 // 4. Make the program read input from file and write output to file.
+// 5. Add double type back in but it seems like a pain to tell difference
 /////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-//#include <string>
-//#include <fstream>
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+union Nums2Swap {
+    int IntNum;
+    float FloatNum;
+};
 
 //Overloading functions to allow for other data types
 int* SwappinNumbers (int Nums[]);
 double* SwappinNumbers (double Nums[]);
 float* SwappinNumbers (float Nums[]);
 
+//File I/O fucntions
+//string FileOpener (string FileName);
+//string FileCreation (string Filename);
+
+//Data Type Tester
+Nums2Swap CheckTypes (string UnknownData);
+
 int main() {
-    int Numbers[2];
-    int input = 0;
+    //Initializing string to find input
+    string User_Input = "";
+
+    //Data to hold number values
+    union Nums2Swap Number1, Number2;
 
     //Asking user to select to open a file or to input data
     cout << "Do you want to open a file or input values?\n";
     cout << "1. Open File\n2. Input Values\nPlease Enter 1 or 2: ";
-    cin >> input;
-    while(!cin || input > 2){
+    cin >> User_Input;
+
+    while(User_Input != "1" && User_Input != "2"){
         cout << "\nPlease enter a valid input.\n\n";
         cout << "Do you want to open a file or input values?\n";
         cout << "1. Open File\n2. Input Values\nPlease Enter 1 or 2: ";
         cin.clear();
         cin.ignore();
-        cin >> input;
+        cin >> User_Input;
     }
-    cout << "You chose " << input << endl;
-    cout << "Enter 1st value: ";
-    cin >> Numbers[0];
-    cout << "Enter 2nd value: ";
-    cin >> Numbers[1];
-    cout << "Values before swapping are:\n" << endl;
-    cout << "x: " << Numbers[0] << " y: " << Numbers[1] << endl;
+    switch (User_Input.at(0)) {
+        case '1': {
+            cout << "What is the name of the file you're trying to open?\n";
+            cin >> User_Input;
+            //FileOpener(User_Input);
+            break;
+        }
+        case '2': {
+            cout << "Enter 1st value: ";
+            cin >> User_Input;
+            Number1 = CheckTypes(User_Input);
+            cout << "Enter 2nd value: ";
+            cin >> User_Input;
+            Number2 = CheckTypes(User_Input);
+            break;
 
-
+        }
+    }
 
     // swapping using third variable
 //    int temp = x;
@@ -97,4 +123,27 @@ double* SwappinNumbers (double Nums[]){
     Nums[1] = Nums[0] - Nums[1];
     Nums[0] = Nums[0] - Nums[1];
     return Nums;
+}
+/*
+string FileOpener (string UserInput) {
+    string NumberRead;
+    ofstream myfile;
+    myfile.open (UserInput, ios::in);
+    if (myfile.is_open()){
+        while(myfile)
+    }
+}
+ */
+
+Nums2Swap CheckTypes (string UnknownData){
+
+    Nums2Swap SelectedData;
+    for(unsigned int i = 0; i < UnknownData.length(); i++){
+        if(UnknownData.at(i) == '.'){
+            SelectedData.FloatNum = stof(UnknownData);
+        }
+        else if(i+1 == UnknownData.length()){
+            SelectedData.IntNum = stoi(UnknownData);
+        }
+    }
 }
